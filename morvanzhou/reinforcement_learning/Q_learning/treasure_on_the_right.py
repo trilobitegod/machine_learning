d5544e1005ba7cfc47940646786539cc85a61463
SnakeT <mygooogle@163.com>
2018-11-25 10:03:04 +0800

    done

--- morvanzhou/reinforcement_learning/Q_learning/treasure_on_the_right.py
+++ morvanzhou/reinforcement_learning/Q_learning/treasure_on_the_right.py
@@ -6,7 +6,7 @@ Created on Sun Nov 25 01:08:30 2018
 """
 
 import numpy as np
-import panda as pdd
+import pandas as pd
 import time
 
 np.random.seed(2)
@@ -20,12 +20,12 @@ MAX_EPISODES = 13
 FRESH_TIME = 0.3
 
 def build_q_table(n_states, actions):
-    table = pd.DataFrame(np.zeros((n_states, length(actions))), column=actions,)
+    table = pd.DataFrame(np.zeros((n_states, len(actions))), columns=actions,)
     return table
 
 def choose_action(state, q_table):
     state_actions = q_table.iloc[state,:]
-    if (np.random.uniform() > EPSILON) or ((stata_actions == 0).all()):
+    if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):
         action_name = np.random.choice(ACTIONS)
     else:
         action_name = state_actions.idxmax()
@@ -47,5 +47,47 @@ def get_env_feedback(S, A):
             S_ = S-1
     return S_, R
 
+def update_env(S, episode, step_counter):
+    # This is how environment be updated
+    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(2)
+        print('\r                                ', end='')
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print('\r{}'.format(interaction), end='')
+        time.sleep(FRESH_TIME)
+        
+def rl():
+    q_table = build_q_table(N_STATES, ACTIONS)
+    for episode in range(MAX_EPISODES):
+        step_counter = 0
+        S = 0
+        is_terminated = False
+        update_env(S, episode, step_counter)
+        while not is_terminated:
+            A = choose_action(S, q_table)
+            S_, R = get_env_feedback(S, A)
+            q_predict = q_table.loc[S, A]
+            if S_ != 'terminal':
+                q_target = R + GAMMA * q_table.iloc[S_, :].max()
+            else:
+                q_target = R
+                is_terminated = True
+            q_table.loc[S, A] += ALPHA * (q_target - q_predict)
+            S = S_
+            update_env(S, episode, step_counter)
+            step_counter += 1
+    return q_table
 
+
+if __name__ == '__main__':
+    q_table = rl()
+    print('\r\nQ-table:\n')
+    print(q_table)
+        
+                
                 
